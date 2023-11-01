41c0827d1c4a845bd281149f7a55230dc1b1f0fb
Danielle Barda <dbarda@redhat.com>
2023-11-01 20:40:53 +0200

    OCM-3165: Create sub-module machine-pool

--- examples/rosa-classic-with-unmanaged-oidc/variables.tf
+++ examples/rosa-classic-with-unmanaged-oidc/variables.tf
@@ -15,3 +15,37 @@ variable "openshift_version" {
 variable "cluster_name" {
   type = string
 }
+
+variable "machine_pool_name" {
+  description = "Name of the machine pool. Must consist of lower-case alphanumeric characters or '-', start and end with an alphanumeric character."
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Identifier of the machine type used by the nodes, for example `m5.xlarge`. Use the `rhcs_machine_types` data source to find the possible values."
+  type        = string
+}
+
+variable "autoscaling_enabled" {
+  description = "Enables autoscaling. If `true`, this variable requires you to set a maximum and minimum replicas range using the `max_replicas` and `min_replicas` variables."
+  type        = bool
+  default     = null
+}
+
+variable "min_replicas" {
+  description = "The minimum number of replicas for autoscaling functionality."
+  type        = number
+  default     = null
+}
+
+variable "max_replicas" {
+  description = "The maximum number of replicas for autoscaling functionality."
+  type        = number
+  default     = null
+}
+
+variable "replicas" {
+  description = "The amount of the machine created in this machine pool."
+  type        = number
+  default     = null
+}
