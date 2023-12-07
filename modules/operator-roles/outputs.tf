f475376a3949a2bc4157819ae7a387cb14fc646a
Nir Argaman <nargaman@redhat.com>
2023-12-07 08:16:27 +0200

    Add output to operator-roles module - operator_roles_arn

--- modules/operator-roles/outputs.tf
+++ modules/operator-roles/outputs.tf
@@ -1,3 +1,7 @@
 output "operator_role_prefix" {
   value = var.operator_role_prefix
 }
+
+output "operator_roles_arn" {
+  value = { for idx, val in aws_iam_role.operator_role : data.rhcs_rosa_operator_roles.operator_roles.operator_iam_roles[idx].operator_namespace => val.arn }
+}
